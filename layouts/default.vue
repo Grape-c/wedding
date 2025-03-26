38ab614cafbb65c771d80c141e1e7760346d3108
Grape-c <sal55960310@gmail.com>
2025-03-26 22:45:41 +0800

    feat: 新增路由

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,25 @@
 <template>
-  <div>
-    <!-- <UDashboardToolbar color="orange"> -->
+  <div class="background-red">
     <UNavigationMenu
       :items="items"
-      highlight
-      class="flex items-center justify-center "
-      color="warning"
-    />
-    <slot /> <!-- 這裡會插入頁面內容 -->
-    <!-- </UDashboardToolbar> -->
+      variant="link"
+      class="flex items-center justify-center custom-navigation-menu"
+    >
+      <template #item="{ item }">
+        <p
+          class="text-white transition-colors tracking-normal hover:text-white hover:font-extrabold hover:tracking-tight mx-4"
+        >
+          <span v-for="(part, index) in splitLabel(item.label)" :key="index" :class="{'font-style-zh': isChinese(part), 'font-style-en': !isChinese(part)}">
+            {{ part }}
+          </span>
+        </p>
+      </template>
+    </UNavigationMenu>
+    <transition name="fade" mode="out-in">
+      <slot :key="$route.fullPath" />
+    </transition>
   </div>
 </template>
-
 
 <script setup lang="ts">
 import type { NavigationMenuItem } from '@nuxt/ui';
@@ -20,17 +28,57 @@ definePageMeta({
 })
 const items = ref<NavigationMenuItem[]>([
   {
-    label: '主頁',
-    icon: 'i-lucide-user',
+    label: '主頁 - HOME',
+    icon: 'i-lucide-house',
+    to: '/HomePage',
     active: true
   },
   {
-    label: 'Members',
-    icon: 'i-lucide-users'
+    label: '活動事項 - Events',
+    icon: 'i-lucide-square-chart-gantt',
+    to: '/eventPage'
   },
   {
-    label: 'Notifications',
-    icon: 'i-lucide-bell'
+    label: '婚禮流程 - Wedding Schedule ',
+    icon: 'i-lucide-flower',
+    to: '/schedulePage'
   }
 ]);
+
+const isChinese = (text: string) => {
+  return /[\u4e00-\u9fa5]/.test(text);
+};
+
+const splitLabel = (label: string) => {
+  return label.split(/(?=[\u4e00-\u9fa5])|(?<=[\u4e00-\u9fa5])/);
+};
 </script>
+
+<style scoped>
+@import url('https://fonts.cdnfonts.com/css/morano');
+@font-face {
+  font-family: 'JasonHandwriting8';
+  src: url('@/assets/fonts/JasonHandwriting8.ttf');
+}
+
+.font-style-zh {
+  font-family: 'JasonHandwriting8', monospace;
+  font-size:  1.5rem;
+}
+
+.font-style-en {
+  font-family: 'morano', sans-serif;
+  font-size:  1.5rem;
+}
+
+.custom-navigation-menu .navigation-menu-item {
+  margin: 0 2rem; /* Adjust the margin to increase spacing between items */
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0;
+}
+</style>
---
+++ pages/EventPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="background-red">event</div>
+</template>
+
+<style scoped>
+.background-red {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #9d2933; /* Change background color to red */
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  z-index: -1; /* Ensure it is at the bottom */
+}
+</style>
