03407a1cb9fd843e7f9c7bf44033a6fba9dd8510
10101598 <tehsin.peng@syntecclub.com.tw>
2025-04-30 16:39:31 +0800

    fix: 會場限制 detail

--- pages/EventPage.vue
+++ pages/EventPage.vue
@@ -238,7 +238,7 @@ const events: Event[] = [
   {
     title: '會場限制',
     icon: 'lucide-building-2',
-    description: '辛志平為百年古蹟，禁止吸煙與攜帶寵物，入場時需脱鞋，建議穿著襪子',
+    description: '辛志平為百年古蹟，禁止吸煙與攜帶寵物，入場時需脱鞋，請穿著襪子',
     details: '為保護古蹟環境，請配合場地規範'
   },
   {
